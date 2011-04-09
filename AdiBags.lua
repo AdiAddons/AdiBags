16fe415754f1a5c352ef8c18015b840c0a87c19d
Adirelle <adirelle@tagada-team.net>
2011-04-09 09:54:48 +0200

    4.1 compatibility.

--- AdiBags.lua
+++ AdiBags.lua
@@ -187,6 +187,10 @@ function addon:OnEnable()
 
 	self:RawHook("OpenAllBags", true)
 	self:RawHook("CloseAllBags", true)
+	if _G.ToggleAllBags then -- 4.1 compat
+		-- TODO: I should really look at 4.1 changes to understand what they are doing
+		self:RawHook("ToggleAllBags", "OpenAllBags", true)
+	end
 	self:RawHook("ToggleBackpack", true)
 	self:RawHook("ToggleBag", true)
 	self:RawHook("OpenBackpack", true)
