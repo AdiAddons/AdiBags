c0f91f580398229a02b985914b90a0da8a892e30
Adirelle <adirelle@gmail.com>
2014-08-09 11:48:01 +0200

    Money: added an option to display a small money frame.

--- modules/MoneyFrame.lua
+++ modules/MoneyFrame.lua
@@ -16,6 +16,17 @@ local mod = addon:NewModule('MoneyFrame', 'AceEvent-3.0')
 mod.uiName = L['Money']
 mod.uiDesc = L['Display character money at bottom right of the backpack.']
 
+function mod:OnInitialize()
+	self.db = addon.db:RegisterNamespace(
+		self.moduleName,
+		{
+			profile = {
+				small = false,
+			}
+		}
+	)
+end
+
 function mod:OnEnable()
 	addon:HookBagFrameCreation(self, 'OnBagFrameCreated')
 	if self.widget then
@@ -32,13 +43,27 @@ end
 function mod:OnBagFrameCreated(bag)
 	if bag.bagName ~= "Backpack" then return end
 	local frame = bag:GetFrame()
-	local widget = CreateFrame("Button", addonName.."MoneyFrame", frame, "MoneyFrameTemplate")
+	local template, size = "MoneyFrameTemplate", 19
+	if self.db.profile.small then
+		template, size = "SmallMoneyFrameTemplate", 13
+	end
+	local widget = CreateFrame("Button", addonName.."MoneyFrame", frame, template)
 	self.widget = widget
-	widget:SetHeight(19)
+	widget:SetHeight(size)
 	widget:RegisterForClicks("RightButtonUp")
 	widget:SetScript('OnClick', function() self:OpenOptions() end)
 	addon.SetupTooltip(widget, { L['Money'], L['Right-click to configure.'] }, "ANCHOR_BOTTOMRIGHT")
-	
-	frame:AddBottomWidget(self.widget, "RIGHT", 50, 19, 19, 0)
+
+	frame:AddBottomWidget(self.widget, "RIGHT", 50, size, size, 0)
 end
 
+function mod:GetOptions()
+	return {
+		small = {
+			name = L['Small'],
+			desc = L['Display a smaller money frame. This setting will take effect on next reload.'],
+			type = 'toggle',
+			order = 10,
+		},
+	}, addon:GetOptionHandler(self, false)
+end
