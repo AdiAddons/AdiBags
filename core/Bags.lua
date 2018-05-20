AdiAddons/AdiBags

commit b4c83379e898977516c1bf12126c2f440ed787c4

fix for wow freezing on blizzard sort

widgets/ItemButton.lua
@@ -265,10 +265,7 @@ end
 --------------------------------------------------------------------------------
 
 function buttonProto:CanUpdate()
-	if not self:IsVisible() or addon.holdYourBreath then
-		return false
-	end
-	return true
+	return self:IsVisible()
 end
 
 function buttonProto:FullUpdate()
@@ -510,7 +507,7 @@ end
 function stackProto:OnShow()
 	self:RegisterMessage('AdiBags_UpdateAllButtons', 'Update')
 	self:RegisterMessage('AdiBags_PostContentUpdate')
-	self:RegisterEvent('ITEM_LOCK_CHANGED')
+	--self:RegisterEvent('ITEM_LOCK_CHANGED')
 	if self.button then
 		self.button:Show()
 	end

commit c7a4c11ad714b5609a06c1278842c29240abb899

Fixed interactivity with merged item stacks.
Fixes #188; caused by b4c8337. Bag sorting will now be performed while closed.

README.textile
@@ -23,7 +23,7 @@ Most of these features may be configured. Check out the configuration panel ! (u
 * Smart section layout.
 * Track new items; use the [N] button resets the "new" flag.
 * Display slot type.
-* Bag sorting: left-click on the [S] button; right-click toggles auto-sorting, which sorts the bag on close (may freeze the game for a few seconds).
+* Bag sorting: click on the [S] button.
 * Hide some sections, using the [V] button.
 * Name-based search.
 * Equipped bags (click on the bag icon in the top left corner),
@@ -48,8 +48,6 @@ h2. Tips & tricks
 h2. Known issues
 
 * *New items detection does not work like it did before 1.8*: Ihis is normal. I switched to the NewItems API that Blizzard has added in patch 6.0. This API does not work for bank nor items coming from the bank or the mailbox, but it does not having the timing issue my code had.
-
-* *Opening the bank or closing my bag cause lag spike*: this is an know issue with Blizzard bag sorting. For now, it is called every time you close your bag with auto-sorting enabled. I am working on a solution to sort bags only if their content actually changed.
 
 * *Protected action/addon blocked error:* they sometimes happen with AdiBags. Even though my effort, I can not reproduce these. Blizzard taint reports are very buggy, often blaming the wrong addon. As such they are pretty much useless.
 

core/Bags.lua
@@ -217,6 +217,7 @@ do
 	function backpack:Sort()
 		PlaySound(SOUNDKIT.UI_BAG_SORTING_01)
 		SortBags()
+		C_Timer.After(1, function() addon:OpenBackpack() end)
 	end
 end
 

widgets/ContainerFrame.lua
@@ -334,6 +334,7 @@ function containerProto:CreateSortButton()
 		"S",
 		10,
 		function()
+			addon:CloseAllBags()
 			self.bagObject:Sort()
 			self.forceLayout = true
 		end,

widgets/ItemButton.lua
@@ -271,7 +271,10 @@ end
 --------------------------------------------------------------------------------
 
 function buttonProto:CanUpdate()
-	return self:IsVisible()
+	if not self:IsVisible() or addon.holdYourBreath then
+		return false
+	end
+	return true
 end
 
 function buttonProto:FullUpdate()
@@ -518,7 +521,7 @@ end
 function stackProto:OnShow()
 	self:RegisterMessage('AdiBags_UpdateAllButtons', 'Update')
 	self:RegisterMessage('AdiBags_PostContentUpdate')
-	--self:RegisterEvent('ITEM_LOCK_CHANGED')
+	self:RegisterEvent('ITEM_LOCK_CHANGED')
 	if self.button then
 		self.button:Show()
 	end