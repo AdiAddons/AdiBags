a545d2f02fbdc41cec5a8591a95671c669f257ee
Talyrius <contact@talyrius.net>
2019-03-18 08:16:15 -0700

    Fixed another error related to the ItemButtonTemplate changes.
    Fixes #339.

--- core/OO.lua
+++ core/OO.lua
@@ -51,12 +51,8 @@ end
 
 local function Class_Create(class, ...)
 	class.serial = class.serial + 1
-	local self
-	if (class.frameTemplate == "ItemButtonTemplate") then
-		self = CreateFrame("ItemButton", addonName..class.name..class.serial, defaultParent)
-	else
-		self = CreateFrame(class.frameType, addonName..class.name..class.serial, defaultParent, class.frameTemplate)
-	end
+	local self = CreateFrame(class.frameType, addonName..class.name..class.serial, defaultParent, class.frameTemplate)
+	self.GetItemContextMatchResult = nil -- We're not using the ContainerFrameItemButtonMixin
 	self:SetParent(nil) -- Get rid of the parent once the OnLoad handler has been called
 	setmetatable(self, class.metatable)
 	self:ClearAllPoints()
