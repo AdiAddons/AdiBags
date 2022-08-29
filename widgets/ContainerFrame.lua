a82665c51f5c1609b420f9c4f34d5ad8eda1645e
Antonio Lobato <aj.lobato@gmail.com>
2022-08-28 23:01:38 -0700

    Fully dynamic column creation

--- widgets/ContainerFrame.lua
+++ widgets/ContainerFrame.lua
@@ -98,8 +98,8 @@ local bagSlots = {}
 function containerProto:GridTest()
 	self.grid = addon:CreateGridFrame("testing")
 	self.grid:DeferUpdate()
---	self.grid:AddColumn()
---	self.grid:AddColumn()
+	--self.grid:AddColumn()
+	--self.grid:AddColumn()
 	self.grid:SetMinimumColumnWidth(120)
 	for i = 1, 3 do
 		local name = string.format("TestGridFrame%d",i)
