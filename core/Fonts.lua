327ae5618fb4faebdee3cba9b7a9827457784d02
neun0eil <neun0eil@nokoda.fr>
2022-10-27 12:32:01 +0200

    Fix: font change error and reduce slider step to 1

--- core/Fonts.lua
+++ core/Fonts.lua
@@ -76,7 +76,7 @@ function proto:SetSetting(info, value, ...)
 			return
 		end
 		db[name] = value
-		self:SetFont(LSM:Fetch(FONT, db.name), db.size)
+		self:SetFont(LSM:Fetch(FONT, db.name), db.size, "")
 	else
 		return
 	end
@@ -164,7 +164,7 @@ function addon:CreateFontOptions(font, title, order)
 				order = 20,
 				min = mediumSize - 8,
 				max = mediumSize + 8,
-				step = 4,
+				step = 1,
 			},
 			color = {
 				name = L['Color'],
